9e765d550b688ea00b687fdef02f3b8f61fdf377
Dennis Healy <dhealy41@gmail.com>
2020-03-13 09:01:17 -0400

    reallly loving this whole github thing

---
+++ TestSQLtoGithub/TestSQLtoGithub/TestTableofLove.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[TestTableofLove]
+(
+    [LoveId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [LoveName] VARCHAR(200) NOT NULL, 
+    [LoveStrength] INT NOT NULL DEFAULT 10, 
+    [LoveCycle] VARCHAR(50) NULL
+)
